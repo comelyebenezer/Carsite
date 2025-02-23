7122f836a06337303c9c4e6dda784194577110e9
unknown <comely.jr@gmail.com>
2025-02-23 06:29:17 -0800

    Landing page

---
+++ src/add-listing/index.jsx
@@ -0,0 +1,30 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+function AddListing() {
+  return (
+    <div>
+        <Header />
+
+        <div className='px-10 md:px-20 my-1'>
+            <h2 className='font-bold text-4xl'> Add New Listing </h2>
+
+            <form action="" className='p-10 border rounded-xl mt-10'>
+                {/* Car Details  */}
+
+                <div>
+                    <h2 className='font-medium text-xl mb-6'>Car Details</h2>
+                </div>
+
+                {/* Features List  */}
+
+
+                {/* Car images  */}
+            </form>
+
+        </div>
+    </div>
+  )
+} 
+
+export default AddListing
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,7 +4,7 @@ import Data from '@/Shared/Data'
 function Category() {
   return (
     <div className='mt-40'>
-        <h2 className='font-bold text-3xl text-center mb-6'>Search By Type</h2>
+        <h2 className='font-bold text-3xl text-center mb-6'>Search Other Tesla</h2>
 
         <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-9 gap-6 px-20'>
 
