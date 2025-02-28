32ebce769f7352d41ded4913bde4ca7b30d5d36a
unknown <comely.jr@gmail.com>
2025-02-28 07:33:02 -0800

    first end front end

--- src/add-listing/components/TextAreaField.jsx
+++ src/add-listing/components/TextAreaField.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Textarea } from "@/components/ui/textarea"
 
 
-function TextAreaField({item}) {
+function TextAreaField({item,handleInputChange}) {
   return (
     <div>
-        <Textarea />
+        <Textarea onChange={(e)=>handleInputChange(item.name,e.target.value)} required={item.required}/>
     </div>
   )
 }
--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,22 +1,39 @@
 import Header from '@/components/Header'
 import { Car } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 import carDetails from './../Shared/carDetails.json'
 import InputField from './components/InputField'
 import DropdownField from './components/DropdownField'
 import TextAreaField from './components/TextAreaField'
 import { Textarea } from '@/components/ui/textarea'
-import { Separator } from '@radix-ui/react-select'
+import { Separator, Value } from '@radix-ui/react-select'
 import features from './../Shared/features.json'
 import { Checkbox } from "@/components/ui/checkbox"
+import { Button } from '@/components/ui/button'
 
 
 function AddListing() {
+
+  const [formData,setFormData] = useState([]);
+
+  const handleInputChange=(name,value)=>{
+      setFormData((prevData)=>({
+        ...prevData,
+        [name]:value
+      }))
+      console.log(formData)
+  }
+
+    const onSubmit=(e)=>{
+      e.preventDefault();
+      console.log(formData)
+    }
+
   return (
     <div>
         <Header />
 
-        <div className='px-10 md:px-20 my-1'>
+        <div className='px-10 md:px-20 my-10'>
             <h2 className='font-bold text-4xl'> Add New Listing </h2>
 
             <form action="" className='p-10 border rounded-xl mt-10'>
@@ -29,9 +46,11 @@ function AddListing() {
                           <div key={index}>
                             <label className='text-sm'> {item.label} {item.required && <span className='text-red-600'>*</span>}</label>
                             {item.fieldType== 'text'|| item.fieldType== 'number'
-                            ?<InputField item={item}/>
-                            :item.fieldType=='dropdown'? <DropdownField item={item}/>
-                            :item.fieldType=='textarea'? <Textarea  item={item}/>
+                            ?<InputField item={item} handleInputChange={handleInputChange}/>
+                            :item.fieldType=='dropdown'
+                            ? <DropdownField item={item} handleInputChange={handleInputChange}/>
+                            :item.fieldType=='textarea'
+                            ? <Textarea  item={item} handleInputChange={handleInputChange}/>
                             : null }
                           </div>
 
@@ -43,10 +62,10 @@ function AddListing() {
                 {/* Features List  */}
                     <div>
                       <h2 className='font-medium text-xl my-6'>Features</h2>
-                      <div>
+                      <div className='grid grid-cols-2 md:grid-cols-3 gap-2'>
                         {features.features.map((item, index)=>(
-                          <div key={index}>
-                              <Checkbox /> <h2>{item.label}</h2>
+                          <div key={index} className='flex gap-2 items-center'>
+                              <Checkbox onCheckedChange={(value)=>handleInputChange(item.name,value)}/> <h2>{item.label}</h2>
 
                           </div>
                         ))}
@@ -54,6 +73,10 @@ function AddListing() {
                     </div>
 
                 {/* Car images  */}
+
+                <div className='mt-10 flex justify-end'>
+                  <Button type="submit" onClick={(e)=>onSubmit(e)}>Submit</Button>
+                </div>
             </form>
 
         </div>
